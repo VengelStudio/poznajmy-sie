9e1a3aee37038f1db7f4c2915f41891653a89cc1
Bartosz Kępka <bartoszkepka1999@gmail.com>
2020-02-08 23:30:31 +0100

    Disable header in MainNavigator

--- App.tsx
+++ App.tsx
@@ -6,12 +6,18 @@ import OptionsPage from './Components/OptionsPage';
 import SpinPage from './Components/SpinPage';
 import QuestionPage from './Components/QuestionPage';
 
-const MainNavigator = createStackNavigator({
-  WelcomePage: {screen: WelcomePage},
-  OptionsPage: {screen: OptionsPage},
-  SpinPage: {screen: SpinPage},
-  QuestionPage: {screen: QuestionPage},
-});
+const MainNavigator = createStackNavigator(
+  {
+    WelcomePage: {screen: WelcomePage},
+    OptionsPage: {screen: OptionsPage},
+    SpinPage: {screen: SpinPage},
+    QuestionPage: {screen: QuestionPage},
+  },
+  {
+    header: null,
+    headerMode: 'none',
+  },
+);
 
 const App = createAppContainer(MainNavigator);
 
