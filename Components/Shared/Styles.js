de9c7506758b340ccc9c7b92e906f29be71f6057
Bartosz Kępka <bartoszkepka1999@gmail.com>
2020-03-07 23:16:36 +0100

    Add shared styles and change desing in all components

---
+++ Components/Shared/Styles.js
@@ -0,0 +1,23 @@
+var React = require('react-native');
+
+var {StyleSheet} = React;
+
+module.exports = StyleSheet.create({
+  Button: {
+    display: 'flex',
+    height: '8%',
+    width: '50%',
+    borderRadius: 40,
+    borderWidth: 2,
+    borderColor: 'transparent',
+    backgroundColor: '#D30C7B',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 140,
+  },
+  ButtonText: {
+    fontFamily: 'simplifica',
+    fontSize: 35,
+    color: 'white',
+  },
+});
