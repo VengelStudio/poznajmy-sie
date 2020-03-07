2f9854bba0ff929ccb5d5d551ecd60427bc1e143
Lukasz Blachnicki <lblachnicki@gmail.com>
2020-03-07 14:29:15 +0100

    Use a different font for questions, fix conflicts, add passing number of players

--- Components/Utilities/methods.ts
+++ Components/Utilities/methods.ts
@@ -1,4 +1,4 @@
-import {Question} from './data.interface';
+import {IGlobalState} from 'Components/Context/context';
 
 export const getRandomColor = () => {
   var letters = '0123456789ABCDEF';
@@ -9,99 +9,27 @@ export const getRandomColor = () => {
   return color;
 };
 
-export const getRandomQuestion = (questions: Question[]) => {
-  if (questions.length > 0) {
-    return questions[Math.floor(Math.random() * questions.length)];
+// get color from HSL wheel
+export const getWheelColor = (id: number) => {
+  var letters = '0F';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 2)];
+  }
+  return color;
+};
+
+export const getRandomQuestion = ({
+  filteredQuestions,
+  currentQuestionId,
+}: IGlobalState) => {
+  console.log(filteredQuestions);
+  console.log(currentQuestionId);
+  if (filteredQuestions.length > 0) {
+    return filteredQuestions.filter(q => {
+      return q.id === currentQuestionId;
+    })[0];
   } else {
     return null;
   }
 };
-
-// export const getRandomColor = () => {
-//   var colors = [
-//     {
-//       colorName: 'Czerwony',
-//       colorCode: '#FF0000',
-//     },
-//     {
-//       colorName: 'Niebieski',
-//       colorCode: '0000FF',
-//     },
-//     {
-//       colorName: 'Zielony',
-//       colorCode: '008000',
-//     },
-//     {
-//       colorName: 'Fioletowy',
-//       colorCode: '#800080',
-//     },
-//     {
-//       colorName: 'Beżowy',
-//       colorCode: '#C2B280',
-//     },
-//     {
-//       colorName: 'Pomarańczowy',
-//       colorCode: '#D4A017',
-//     },
-//     {
-//       colorName: '#Różowy',
-//       colorCode: '#C48793',
-//     },
-//     {
-//       colorName: 'Szary',
-//       colorCode: '#808080',
-//     },
-//     {
-//       colorName: 'Brązowy',
-//       colorCode: '#964B00',
-//     },
-//     {
-//       colorName: 'Morski',
-//       colorCode: '#008080',
-//     },
-//     {
-//       colorName: 'Oliwkowy',
-//       colorCode: '#808000',
-//     },
-//     {
-//       colorName: 'Czerwony',
-//       colorCode: '#FF0000',
-//     },
-//     {
-//       colorName: 'Czerwony',
-//       colorCode: '#FF0000',
-//     },
-//     {
-//       colorName: 'Czerwony',
-//       colorCode: '#FF0000',
-//     },
-//     {
-//       colorName: 'Czerwony',
-//       colorCode: '#FF0000',
-//     },
-//     {
-//       colorName: 'Czerwony',
-//       colorCode: '#FF0000',
-//     },
-//     {
-//       colorName: 'Czerwony',
-//       colorCode: '#FF0000',
-//     },
-//     {
-//       colorName: 'Czerwony',
-//       colorCode: '#FF0000',
-//     },
-//     {
-//       colorName: 'Czerwony',
-//       colorCode: '#FF0000',
-//     },
-//     {
-//       colorName: 'Czerwony',
-//       colorCode: '#FF0000',
-//     },
-//   ];
-//   var randomNumber = Math.floor(Math.random() * 20);
-//   var name = colors[randomNumber].colorName;
-//   var code = colors[randomNumber].colorCode;
-//   return code;
-// };
